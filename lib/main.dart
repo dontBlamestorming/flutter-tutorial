00ed226c7e7b6c7468ff83dceaac072ca25c4a9f
dontBlamestorming <lklone2005@gmail.com>
2022-01-04 14:27:44 +0900

    add Home

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,25 @@ class TutorialApps extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const PracticeUIApp());
+        home: const Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Home Page"),
+      ),
+    );
   }
 }
 
